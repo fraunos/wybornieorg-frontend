d662051ac5510d7cea591220bbdf1acec1026782
Michal Woloszyn <fraunos@gmail.com>
2017-08-27 13:13:13 +0200

    fix vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
-Vue.use(Vuex)
 import store from './store'
 
 Vue.config.productionTip = false
