dd20f636e39217c28eafbde01873b5d7ec690846
Michal Woloszyn <fraunos@gmail.com>
2017-08-27 18:42:11 +0200

    dodanie strony about

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="about">
+  <h1>O stronie</h1>
+  <h3>Hej!</h3>
+  <p>Strona w budowie</p>
+</div>
+</template>
+
+<script>
+
+import Projects from '@/components/Projects'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    Projects
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Main from '@/components/Main'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: Main
+    },
+    {
+      path: '/ostronie',
+      name: 'about',
+      component: About
     }
   ]
 })
