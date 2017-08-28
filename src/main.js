0dfae6d8db3d70c4163f71bf7337810223d24abf
Michal Woloszyn <fraunos@gmail.com>
2017-08-28 14:11:01 +0200

    zamiana feather-icons CDN na vue-feather-icon

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueFeatherIcon from 'vue-feather-icon'
+
+Vue.use(VueFeatherIcon)
 
 Vue.config.productionTip = false
 
