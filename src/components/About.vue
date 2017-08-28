5eaa1f0a62a806fc568f4a62064c77fd2c345535
Michal Woloszyn <fraunos@gmail.com>
2017-08-28 17:50:05 +0200

    nowe info w 'o stronie'

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,20 +2,18 @@
 <div class="about">
   <h1>O stronie</h1>
   <h3>Hej!</h3>
-  <p>Strona w budowie</p>
+  <p>Wszystkie dane wyświetlane na tej stronie są dostępne publicznie pod  <a href="http://www.sejm.gov.pl/Sejm8.nsf/page.xsp/przeglad_projust">tym adresem</a> oraz na odpowiednich podstronach.</p>
+  <p>Autor: <a href="https://fraunos.github.io">Michał Wołoszyn</a></p>
 </div>
 </template>
 
 <script>
-
-import Projects from '@/components/Projects'
 
 export default {
   data () {
     return {}
   },
   components: {
-    Projects
   }
 
 }
