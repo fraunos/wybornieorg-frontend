f6039f18049766a2a945c14968260c04f8642038
Michal Woloszyn <fraunos@gmail.com>
2017-08-28 23:43:43 +0200

    troche tekstu

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,12 @@
 <div class="about">
   <h1>O stronie</h1>
   <h3>Hej!</h3>
+  <p>Dużo zastanawiałem się w jaki sposób mógłbym sprawić, by świat był nieco lepszy. Zainteresowałem się polityką, ale zaobserwowałem, że niestety tam gdzie jest władza i pieniądze tam są konflikty. Skoro jednak mamy demokrację postanowiłem wymyśleć sposób, by ją usprawnić, pozwolić ludziom lepiej dokonywać wyborów przy urnie, i tak powstało <a href="/">wybornie.org</a>!</p>
+  <p>Doszedłem do wniosku, że ludzie mają zbyt mało czasu i zbyt krótką pamięć, by śledzić efektywnie scenę polityczną. Do tego dochodzą emocje, które dodatkowo zaburzają racjonalne wybory. Jest jednak obiektywne kryterium wyboru - rozliczenie posłów z ich głosowań.</p>
+  <p>Dzięki tej stronie użytkownicy mogą wybierać projekty uchwalonych ustaw i dowiedzieć się który z posłów zagłosował tak jak użytkownik by chciał. W ten sposób jak na dłoni widać którzy posłowie są prawdziwymi reprezentantami.</p>
+  <p>Niestety ze względu na dużą rolę partii politycznej w głosowaniu i zbyt małej ilości niezależnych posłów ten portal nie będzie wywierał takiego wpływu, gdyby delegaci do sejmu byli wybierani w innej ordynacji wyborczej.</p>
+  <p></p>
   <p>Wszystkie dane wyświetlane na tej stronie są dostępne publicznie pod  <a href="http://www.sejm.gov.pl/Sejm8.nsf/page.xsp/przeglad_projust">tym adresem</a> oraz na odpowiednich podstronach.</p>
-  <p>Autor: <a href="https://fraunos.github.io">Michał Wołoszyn</a></p>
+  <p>Kod źródłowy jest otwarty, a linki znajdują się w stopce.</p>
 </div>
 </template>
