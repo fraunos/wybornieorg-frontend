2d11937a66b95a5c5a4c206ea179a628c87f0cc1
Michał Wołoszyn <fraunos@gmail.com>
2017-10-16 23:09:52 +0100

    bezpłciowy tekst

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="hello">
   <h1>Witaj!</h1>
-  <p>Znalazłeś się na stronie <a href="#">wybornie.org</a>! Jest to aplikacja poświęcona wyborom. Jeśli tu jesteś to za pewne zastanawiasz się na kogo zagłosować? Kto jest godny Twojego zaufania? Właśnie w tym postaram się Ci pomóc!</p>
+  <p>Znajdujesz się na stronie <a href="#">wybornie.org</a>! Jest to aplikacja poświęcona wyborom. Jeśli tu jesteś to za pewne zastanawiasz się na kogo zagłosować? Kto jest godny Twojego zaufania? Właśnie w tym postaram się Ci pomóc!</p>
   <p>Dzięki tej aplikacji możesz głosować zupełnie jak posłowie. Wystarczy znaleźć interesujące Cię projekty ustaw i wybrać głos 'Za' lub 'Przeciw'. Następnie, po przejściu w zakładkę Statystyk program wyświetli wyniki - w kolejności - z którym z posłów zgadzasz się najbardziej!</p>
   <p>Powodzenia i rozsądnych wyborów!</p>
   <h3>Wybornie!</h3>
