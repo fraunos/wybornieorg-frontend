894f295369cb308ded80c2660d74f696c4819959
Michał Wołoszyn <fraunos@gmail.com>
2017-10-17 19:22:33 +0100

    dodano listy z zadaniami z trello

--- README.md
+++ README.md
@@ -5,18 +5,4 @@ To repozytorium zawiera kod, z którego generowana jest aplikacja uruchamiana po
 Aplikacja wyświetla dane pobrane z serwera - [backend](https://github.com/fraunos/wybornieorg-backend).
 
 
-TODO:
-- [x] głosowanie na projekty z wyświetlaniem wyników
-- [ ] persistent storage - zapisywanie pobranych z serwera i od użytkownika danych w localStorage cache (dodanie do backendu daty aktualizacji, żeby sprawdzać kiedy należy pobrać ponownie dane na klienta)
-- [ ] sortowanie i wyszukiwanie projektów (po frekwencji, numerze, dacie głosowania, liczbie oddanych głosów)
-- [x] sortowanie statystyk posłów (wg. zgodności)
-- [ ] opisy projektów (nazwy zwyczajowe i artykuły w sieci)
-- [ ] frekwencja posłów
-- [ ] szybka ankieta - najbardziej popularne projekty
-- [x] widok układu parlamentu zamiast okręgów
-- [ ] zaznaczanie projektów, na które został oddany głos
-- [ ] responsywność - dostosowanie do różnych ekranów
-- [x] lepszy wygląd (nowe logo, struktura aplikacji)
-- [ ] portrety posłów
-- [ ] animacje przy głosowaniu i ładowaniu statystyk
-- [x] zaznacznie nieobecności na szaro, wstrzymania się na fioletowo
+TODO: http://wybornie.org/#/ostronie
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="about">
-  <h1>O stronie!</h1>
+  <h1>O stronie <a href="#">wybornie.org</a>!</h1>
   <p>Aplikacja pobiera wszystkie projekty (uchwalone lub odrzucone) ze strony <a target="_blank" href="http://www.sejm.gov.pl/Sejm8.nsf/page.xsp/przeglad_projust">http://www.sejm.gov.pl/Sejm8.nsf/page.xsp/przeglad_projust</a>, a następnie pozwala zagłosować na nie wyświetlając przy tym wszelkie zebrane informacje, by ułatwić wybór. Po zagłosowaniu na wybrane przez siebie projekty użytkownik może przejść do podstrony Statystyki, by zobaczyć z którymi z posłów i ugrupowań najbardziej się zgadza.</p>
   <p>Autor strony przyłożył wszelkich starań przy projektowaniu aplikacji by zachować:
   <ul>
@@ -8,26 +8,65 @@
     <li>obiektywność - aplikacja wyświetla i przetwarza tylko publiczne dane</li>
     <li></li>
   </ul></p>
-  <a href="#">wybornie.org</a>
+
+  <h2><a target="_blank" href="https://trello.com/b/EEORaGl3/wybornieorg">Postęp prac</a></h2>
+  <div id="trello">
+    <div id="dozrobienia" class="trello-list">
+      <h3>W trakcie</h3>
+      <ul>
+        <li v-for="item in todo">{{item.name}}</li>
+      </ul>
+    </div>
+    <div id="ukonczono" class="trello-list">
+      <h3>Ukończono</h3>
+      <ul>
+        <li v-for="item in done">{{item.name}}</li>
+      </ul>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      todo: null,
+      done: null
+    }
   },
-  created () {},
+  created () {
+    this.fetchTrelloData()
+  },
   components: {},
   methods: {
-
+    fetchTrelloData () {
+      this.$http.get('https://api.trello.com/1/boards/59e5eea27c51ae318c5fb7a2/cards').then(response => {
+        this.todo = response.body.filter((item) => {
+          return item.idList === '59e5f37e78966b943012e1d2'
+        })
+        this.done = response.body.filter((item) => {
+          return item.idList === '59e5f3835d4cb88f28d42234'
+        })
+      }, response => {
+        // error callback
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
-  font-size: 150%;
+#trello{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.trello-list + .trello-list{
+  margin-left: 2em;
+}
+a{
+  white-space:nowrap;
 }
 </style>
