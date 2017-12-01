a4bde8f9b5dba044df2c47f1c9404ba3dfa154cc
Michał Wołoszyn <fraunos@gmail.com>
2017-12-01 14:30:28 +0100

    standardy PWA

---
+++ static/img/icons/icon generator.sh
@@ -0,0 +1,17 @@
+inkscape -e mstile-150x150.png -a=0:0:512:512 -w=150 logo.svg
+inkscape -e msapplication-icon-144x144.png -a=0:0:512:512 -w=144 logo.svg
+inkscape -e favicon-32x32.png -a=0:0:512:512 -w=32 logo.svg
+inkscape -e favicon-16x16.png -a=0:0:512:512 -w=l6 logo.svg
+inkscape -e favicon.ico -a=0:0:512:512 -w=48 logo.svg
+inkscape -e favicon-16x16.png -a=0:0:512:512 -w=152 logo.svg
+inkscape -e apple-touch-icon-180x180.png -a=0:0:512:512 -w=180 logo.svg
+inkscape -e apple-touch-icon-152x152.png -a=0:0:512:512 -w=152 logo.svg
+inkscape -e apple-touch-icon-120x120.png -a=0:0:512:512 -w=120 logo.svg
+inkscape -e apple-touch-icon-76x76.png -a=0:0:512:512 -w=76 logo.svg
+inkscape -e apple-touch-icon-60x60.png -a=0:0:512:512 -w=60 logo.svg
+inkscape -e apple-touch-icon.png -a=0:0:512:512 -w=180 logo.svg
+inkscape -e android-chrome-512x512.png -a=0:0:512:512 -w=512 logo.svg
+inkscape -e android-chrome-192x192.png -a=0:0:512:512 -w=192 logo.svg
+
+inkscape -e ../logo.png -a=0:0:512:512 -w=512 logo.svg
+inkscape -e ../logo-name.png logo.svg
