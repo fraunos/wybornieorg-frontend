98b6c05c8da0b44d9634a4b2a341a145d7510796
Michał Wołoszyn <fraunos@gmail.com>
2018-01-04 17:35:09 +0100

    ups, zapomnialem o tym pliku

---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <!-- <h1>Wczytywanie danych, proszę czekać</h1> -->
+</div>
+</template>
+
+<script>
+
+export default {
+  props: ['dane'],
+  created () {
+    this.$store.commit('loadSavedData', JSON.parse(window.atob(this.dane)))
+    // browser.bookmarks.create({title: 'wybornie.org', url: location.href})
+  },
+  methods: {
+  }
+}
+</script>
